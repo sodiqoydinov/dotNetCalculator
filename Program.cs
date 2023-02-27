e059bdefd1867d7b07a2b325145d9714af5f123e
Sodiq <sodiq7898@gmail.com>
2023-02-27 09:30:38 +0500

    Calculator_dotNet

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//  My first Project in C#, (.Net) :)
+Console.Write("Enter first number: ");
+decimal number1 = Convert.ToInt64(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+decimal number2 = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Enter operator: (* / + -): ");
+char arithmetic = Convert.ToChar(Console.ReadLine());
+
+switch(arithmetic)
+{
+    case '*':
+        Console.WriteLine($"{number1} * {number2} = {number1*number2}");
+        break;
+    
+    case '/':
+        Console.WriteLine($"{number1} / {number2} = {number1/number2}");
+        break;
+
+    case '+':
+        Console.WriteLine($"{number1} + {number2} = {number1+number2}");
+        break;
+    
+    case '-':
+        Console.WriteLine($"{number1} - {number2} = {number1-number2}");
+        break;
+    
+    default:
+        Console.WriteLine("Error");
+        break;
+}
